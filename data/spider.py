0e399d525e0c53ffc8ec92cf1a125aed4138b690
matt <l3acon@gmail.com>
2018-03-17 17:36:21 -0600

    ingesting works

--- data/spider.py
+++ data/spider.py
@@ -2,7 +2,7 @@ import logging
 import scrapy
 from scrapy.crawler import CrawlerProcess
 
-from datetime import datetime
+import datetime
 from elasticsearch import Elasticsearch
 
 es = Elasticsearch(['http://elastic:supersecret@localhost:9200'])
@@ -12,26 +12,31 @@ class CraigSpider(scrapy.Spider):
     name = 'craigspider'
     base_url='https://albuquerque.craigslist.org/search/sss?s='
     start_urls=list()
-    for i in range(0, 1):
+    for i in range(0, 10):
         start_urls.append(base_url+str(i*120))
 
     def parse(self, response):
         for post in response.css('p.result-info'):
             doc = dict()
             try:
-
-                post_time = str(post.css('time').xpath('@datetime').extract_first()),
-                timestamp=datetime.datetime.strptime(post_time, '%Y-%m-%d %H:%M').isoformat()
+                post_time = datetime.datetime.strptime(str(post.css('time').xpath('@datetime').extract_first()), '%Y-%m-%d %H:%M')
+                timestamp=post_time.isoformat()
+                this_index=str(es_index)+'-'+str(post_time.month)+'-'+str(post_time.day)
                 doc = {
                         'title': str(post.css('a').xpath('text()').extract_first()),
                         'url': str(post.css('a').xpath('@href').extract_first()),
-                        'price:': str(post.css('.result-price').xpath('text()').extract_first())
+                        'price:': str(post.css('.result-price').xpath('text()').extract_first()),
+                        '@timestmp': timestamp
                 }
             except Exception as e:
                 logging.log(logging.WARN, 'failed to get doc: '+repr(e))
                 continue
             logging.log(logging.DEBUG, 'ingesting doc: '+str(doc))
-            res = es.create(index=es_index, timestamp=timestamp, body=doc)
+            if not es.indices.exists(this_index):
+                logging.log(logging.DEBUG, 'creating index: '+str(this_index))
+                res = es.indices.create(index=this_index)
+                logging.log(logging.DEBUG, str(res))
+            res = es.index(index=this_index, doc_type='post', body=doc)
             logging.log(logging.DEBUG, str(res))
 
 process = CrawlerProcess({
@@ -40,7 +45,7 @@ process = CrawlerProcess({
 
 process.crawl(CraigSpider)
 process.start()
-res = es.search(index=es_index, body={'query': {'match_all': {}}})
+res = es.search(index=es_index+'*', body={'query': {'match_all': {}}})
 print('Got %d Hits:' % res['hits']['total'])
 for hit in res['hits']['hits']:
     print('hit: '+str(hit['_source']))
--- data/test.py
+++ data/test.py
@@ -2,21 +2,46 @@ from datetime import datetime
 from elasticsearch import Elasticsearch
 
 es = Elasticsearch(['http://elastic:supersecret@localhost:9200'])
+timestamp=datetime.strptime('2018-03-17 11:42', '%Y-%m-%d %H:%M').isoformat()
+es_index="test-new-index"+str(timestamp)
 
-doc = {
-        'price': '$120',
-        'url': 'something.carigslist.org',
-        'text': 'Jackson JS3 Concert Bass',
-        'timestamp': '2018-03-17 11:42'
-    }
+#doc = {
+#        'price': '$120',
+#        'url': 'something.carigslist.org',
+#        'text': 'Jackson JS3 Concert Bass',
+#        'timestamp': '2018-03-17 11:42'
+#    }
+#
+#res = es.index(index="test-index", doc_type='post', body=doc)
+#print(res)
+#
+#es.indices.refresh(index="test-index")
+#
+#res = es.search(index="test-index", body={"query": {"match_all": {}}})
+#print("Got %d Hits:" % res['hits']['total'])
+#for hit in res['hits']['hits']:
+#        print(hit["_source"])
+#
+if not es.indices.exists(index=es_index):
+    print('creating index: '+str(es_index))
+    es.indices.create(index=es_index, ignore=400)
+    es.index(
+            index=es_index, 
+            doc_type="test-type", 
+            body = {
+                    'price': '$120',
+                    'url': 'something.carigslist.org',
+                    'text': 'Jackson JS3 Concert Bass',
+                    'timestamp': '2018-03-17 11:42'
+                }
+            )
+else:
+  print('index already created: '+str(es_index))
 
-res = es.index(index="test-index", doc_type='post', body=doc)
-print(res)
+print('refreshig indeices')
+es.indices.refresh(index=es_index)
+res = es.search(index=es_index, body={'query': {'match_all': {}}})
 
-es.indices.refresh(index="test-index")
-
-res = es.search(index="test-index", body={"query": {"match_all": {}}})
-print("Got %d Hits:" % res['hits']['total'])
+print('Got %d Hits:' % res['hits']['total'])
 for hit in res['hits']['hits']:
-        print(hit["_source"])
-
+    print('hit: '+str(hit['_source']))
