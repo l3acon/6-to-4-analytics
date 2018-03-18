94c7f7bb88184573f434c332f39d33f511558a8d
matt <l3acon@gmail.com>
2018-03-17 22:03:35 -0600

    adding custom mappings

--- data/spider.py
+++ data/spider.py
@@ -1,31 +1,49 @@
 import logging
 import scrapy
+import hashlib
 from scrapy.crawler import CrawlerProcess
 
 import datetime
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch(['http://elastic:supersecret@localhost:9200'])
+es = Elasticsearch(['http://elastic:supersecret@muffuletta:9200'])
 es_index='cl-post'
+
+mappings='''
+{
+  "mappings": {
+    "doc": {
+        "properties": {
+            "title": { "type": "text", "fielddata": "true" },
+            "url": { "type": "text" },
+            "price": { "type": "float" }
+        }
+    }
+  }
+}
+'''
 
 class CraigSpider(scrapy.Spider):
     name = 'craigspider'
     base_url='https://albuquerque.craigslist.org/search/sss?s='
     start_urls=list()
-    for i in range(0, 10):
+    for i in range(0, 25):
         start_urls.append(base_url+str(i*120))
 
     def parse(self, response):
+        h=hashlib.new('sha1')
         for post in response.css('p.result-info'):
             doc = dict()
             try:
                 post_time = datetime.datetime.strptime(str(post.css('time').xpath('@datetime').extract_first()), '%Y-%m-%d %H:%M')
                 timestamp=post_time.isoformat()
+                title=str(post.css('a').xpath('text()').extract_first())
                 this_index=str(es_index)+'-'+str(post_time.month)+'-'+str(post_time.day)
+                h.update(timestamp+title)
                 doc = {
-                        'title': str(post.css('a').xpath('text()').extract_first()),
+                        'title': title,
                         'url': str(post.css('a').xpath('@href').extract_first()),
-                        'price:': str(post.css('.result-price').xpath('text()').extract_first()),
+                        'price': str(post.css('.result-price').xpath('text()').extract_first()).replace('$', ''),
                         '@timestmp': timestamp
                 }
             except Exception as e:
@@ -34,9 +52,9 @@ class CraigSpider(scrapy.Spider):
             logging.log(logging.DEBUG, 'ingesting doc: '+str(doc))
             if not es.indices.exists(this_index):
                 logging.log(logging.DEBUG, 'creating index: '+str(this_index))
-                res = es.indices.create(index=this_index)
+                res = es.indices.create(index=this_index, body=mappings)
                 logging.log(logging.DEBUG, str(res))
-            res = es.index(index=this_index, doc_type='post', body=doc)
+            res = es.index(index=this_index, id=h.hexdigest(), doc_type='doc', body=doc)
             logging.log(logging.DEBUG, str(res))
 
 process = CrawlerProcess({
